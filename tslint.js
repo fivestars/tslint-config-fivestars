7eb61a92c44078ea5d629d435d0e21ce58036d67
Rob Corell <rob@robcorell.com>
2018-09-24 02:39:06 -0500

    Add basic overrides

--- tslint.js
+++ tslint.js
@@ -1,14 +1,22 @@
 module.exports = {
     extends: [
         'tslint-config-airbnb',
-        'tslint-react',
-
+        'tslint-react'
     ],
     rules: {
         'function-name': null,
+        'max-line-length': [true, 120],
         'member-access': false,
         'object-shorthand-properties-first': false,
         'quotemark': [true, 'single', 'jsx-double'],
+        'semicolon': [true, 'always', 'ignore-bound-class-methods'],
+        'ter-indent': [
+            true,
+            4, {
+                'SwitchCase': 1
+            }
+        ],
+        'trailing-comma': [true, {'multiline': 'never', 'singleline': 'never'}],
         'variable-name': [true, 'ban-keywords', 'allow-leading-underscore']
     }
 };
