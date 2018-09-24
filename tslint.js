2df4d4bca8c265f25124140fd74d74622f0ff583
Rob Corell <rob@robcorell.com>
2018-09-24 01:22:41 -0500

    Move existing instore rules into this config

--- tslint.js
+++ tslint.js
@@ -1,8 +1,14 @@
 module.exports = {
     extends: [
-        'tslint-config-airbnb'
+        'tslint-config-airbnb',
+        'tslint-react',
+
     ],
     rules: {
-        'object-shorthand-properties-first': false
+        'function-name': null,
+        'member-access': false,
+        'object-shorthand-properties-first': false,
+        'quotemark': [true, 'single', 'jsx-double'],
+        'variable-name': [true, 'ban-keywords', 'allow-leading-underscore']
     }
 };
